16770cea07300344e5c89289b824de9a69e9435b
Shashwat Gupta <shashwatsatna@gmail.com>
2026-03-23 21:13:52 +0530

    feat: add RandomSuffix to strutil for unique slug generation

--- strutil/slug.go
+++ strutil/slug.go
@@ -3,6 +3,8 @@ package strutil
 import (
 	"regexp"
 	"strings"
+
+	"github.com/google/uuid"
 )
 
 var slugRegex = regexp.MustCompile(`[^a-z0-9]+`)
@@ -14,4 +16,10 @@ func Slugify(s string) string {
 	s = strings.ToLower(s)
 	s = slugRegex.ReplaceAllString(s, "-")
 	return strings.Trim(s, "-")
+}
+
+// RandomSuffix returns a short (5-character) random string based on a UUID.
+// This is useful for creating unique slugs or identifiers when collisions occur.
+func RandomSuffix() string {
+	return uuid.New().String()[:5]
 }
